18941559cff1cbe4f73099cfa59e7637cb925818
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2020-01-02 23:45:59 -0800

    Add & Delete working

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const db = require("./data/hubs-model.js"); // imports the database file
 
 const server = express(); // creates the server
 
+server.use(express.json()); // add this to make POST & PUT work 
+                            // needed to parse JSOnN from the body
+
 server.get('/', (req, res) => {
     res.send({ api: "up & running!"})
 });
-
-
 
 // the R in CRUDS (Read)
 server.get('/hubs', (req, res) => {
@@ -24,8 +25,44 @@ server.get('/hubs', (req, res) => {
     })
 });
 
+// the C in CRUDS (Create)
+// POST add a hub
+server.post('/hubs', (req, res) => {
+    // get the data the client sent
+    const hubData = req.body; // express does not know how to parse JSON
+    console.log('hub data', hubData);
+    // call the database & ADD the hub
+  db.add(hubData)
+    .then(hub => {
+        res.status(201).json(hub);
+    })
+    .catch(error => {
+        console.log("error on POST /hubs", error);
+        res.status(500).json({ message: 'error adding the hub' });
+    });  
+});
 
 
+
+// DELETE in CRUDS 
+// Remove a hub by its id
+server.delete('/hubs/:id', (req, res) => {
+    const id = req.params.id;
+
+  db.remove(id)
+    .then(removed => {
+        if (removed) {
+        res.status(200).json({ message: "hubs removed successfully"});
+        } else {
+            // there was no hub with that id
+            res.status(404).json({ message: "hub not found"});
+        }
+    })
+    .catch(error => {
+        console.log("error on DELETE /hubs/:id", error);
+        res.status(500).json({ message: 'error removing the hub' });
+    });  
+}); 
 
 // watch for connections on port 4000
 const port = 4000;
